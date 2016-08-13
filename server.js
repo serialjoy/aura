9b92cc7952f4dedc1e5cf9fe9477efef7d47985a
Cristian Olarasu <cristian.olarasu@gmail.com>
2016-08-13 09:06:44 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,12 @@
+// Reguire modules
+var express = require('express');
+var http_auth = require('express-http-auth');
+
+// Create app
+var app = express();
+
+// Serve static files as default
+app.get('/*', express.static(__dirname + '/peer.framer'));
+
+// Listen for both Heroku and local. Access locally as http://localhost:3333
+app.listen(process.env.PORT || 3333);
