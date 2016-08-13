19798cccabd2dbde7d96712e4d78c58327d72cdd
Cristian <cristian.olarasu@gmail.com>
2016-08-13 09:09:10 +0300

    folder names

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var http_auth = require('express-http-auth');
 var app = express();
 
 // Serve static files as default
-app.get('/*', express.static(__dirname + '/peer.framer'));
+app.get('/*', express.static(__dirname + '/aura.main'));
 
 // Listen for both Heroku and local. Access locally as http://localhost:3333
 app.listen(process.env.PORT || 3333);
